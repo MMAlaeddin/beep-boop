42f13345175b74bc96873caa6e2442771778bd00
Student <example@example.com>
2020-01-24 12:54:29 -0800

    make a working for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,23 @@ $(document).ready(function(){
 
     var userInput = parseInt($("input#number").val());
 
-    var numberInput = ("" + userInput).split("");
+    var numberInput = [];
 
-    console.log(numberInput);
+    for (var i = 0; i <= userInput; i ++) {
+      numberInput.push(i);
+      console.log(numberInput);
+    }
+       
+    
 
     if (isNaN(userInput)) {
       alert("Enter a number!");
-      // console.log(userInput);
+      
 
-
-    // } else {
-    //   var inputArray = [];
-    //   for (var i = 0; i <= userInput; i ++) {
-    //     inputArray.push(i);
-    //     console.log(inputArray);
-    //   }
-    // }
-    // if(inputArray.includes(3)) {
-    //   $("#response").text("beep");
-    var blah = [];
-    } else {
-      for(var index = 0; index <= userInput; index ++) {
-        blah.push(i);
-        console.log(blah);
-      }
     }
 
-    if (blah.includes(1)) {
-      ("#response").text("beep");
-    }
+    
+
 
 
     
