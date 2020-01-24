e69ababfd9b0c3a397c557d4d15884b82b128085
Student <example@example.com>
2020-01-24 11:49:48 -0800

    create alert for non numerical values

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = $("input#number").val();
 
-    var numberInput = userInput.split("");
+    var userInput = parseInt($("input#number").val());
+
+    var numberInput = ("" + userInput).split("");
     console.log(numberInput);
+    if (isNaN(userInput)) {
+      alert("Enter a number!");
+      console.log(userInput);
+
+    // } else {
+    //   var inputArray = [];
+    //   for (var i = 0; i <= userInput; i ++) {
+    //     inputArray.push(i);
+    //     console.log(inputArray);
+    //   }
+    // }
+    // if(inputArray.includes(3)) {
+    //   $("#response").text("beep");
+      
+    }
 
 
-    numberInput.find("1");
-      return ("beep");
     
     
     
@@ -21,7 +35,6 @@ $(document).ready(function(){
 
 
 
-
+  });
 
   });
-});
