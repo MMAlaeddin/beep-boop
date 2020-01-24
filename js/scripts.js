7e40b5fff2653f5cf058a7e4f509498399189b22
Student <example@example.com>
2020-01-24 09:04:10 -0800

    add first set of logic for 1

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    var potato = parseInt($("input#number"));
+
+    if(potato === 1);
+    $("#response").text("beep"); 
+
+
+
+
+  });
+});
